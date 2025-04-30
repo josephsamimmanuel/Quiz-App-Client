7cc4c3d951a56a7440a373ba70d88a122baddbc2
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-30 17:10:25 +0530

    Update BASE_URL in constants.js for environment-specific API endpoints
    - Modified BASE_URL to dynamically switch between localhost and production server based on the current hostname, improving local development experience.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL = "https://quiz-app-server-5u1d.onrender.com/api";
+export const BASE_URL = window.location.hostname === 'localhost' 
+    ? "http://localhost:7777/api" 
+    : "https://quiz-app-server-5u1d.onrender.com/api";
 
 export const REGISTER_USER = `${BASE_URL}/users/register`;
 export const LOGIN_USER = `${BASE_URL}/users/login`;
