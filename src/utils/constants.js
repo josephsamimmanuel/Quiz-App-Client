6c4874ba5402777e16cfee84e2cb63282987ac34
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-30 13:48:34 +0530

    Update BASE_URL in constants.js to use production server URL
    - Commented out the local development BASE_URL and replaced it with the production URL for the quiz app server.
    - Ensured that API calls will now point to the correct server in the deployed environment.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL = "http://localhost:7777/api";
+// export const BASE_URL = "http://localhost:7777/api";
+export const BASE_URL = "https://quiz-app-server-5u1d.onrender.com";
+
 
 export const REGISTER_USER = `${BASE_URL}/users/register`;
 export const LOGIN_USER = `${BASE_URL}/users/login`;
